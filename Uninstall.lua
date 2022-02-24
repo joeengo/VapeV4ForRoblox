8f36530dccac20f73e4346ae9c849a7a0bef715e
engo <63791277+joeengo@users.noreply.github.com>
2022-02-24 12:39:45 +0000

    Create Uninstall.lua

---
+++ Uninstall.lua
@@ -0,0 +1,8 @@
+--// run this file to un install engovape.
+
+-- !! THIS WILL DELETE ANY CUSTOM MODULES/FEATURES YOU HAVE MADE!!! DM ME ON DISCORD TO LEARN HOW TO KEEP THESE FEATURES engo#0320
+
+if isfolder("vape/CustomModules") then 
+  delfolder("vape/CustomModules")
+  print("Successfully uninstalled engovape")
+end
